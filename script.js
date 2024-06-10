95b281b259c6c90644371dcb2f5d23529690bc66
SlubblesThuggy <65078309+SlubblesThuggy@users.noreply.github.com>
2024-06-10 18:21:35 +0200

    Added button for generating a new board

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
 const containerDiv = document.querySelector("#container");
+const generateButton = document.querySelector("#generate");
 
-function generateBoard(width, height) {
-    for (let row = 0; row < height; row++) {
+function generateBoard(size) {
+    // remove old board (should there be one)
+    while (containerDiv.lastElementChild) {
+        containerDiv.removeChild(containerDiv.lastElementChild);
+    }
+
+    // generate new board
+    for (let row = 0; row < size; row++) {
         let gridRowDiv = document.createElement("div");
         gridRowDiv.className = "grid-row";
+
         containerDiv.appendChild(gridRowDiv);
 
-        for (let col = 0; col < width; col++) {
+        for (let col = 0; col < size; col++) {
             let gridCellDiv = document.createElement("div");
             gridCellDiv.className = "grid-cell";
-            gridCellDiv.addEventListener("mouseenter", onGridCellMouseEnter);
+            gridCellDiv.addEventListener("mouseenter", (event) => 
+                event.target.style.backgroundColor = "black"
+            );
+
             gridRowDiv.appendChild(gridCellDiv);
         }
     }
 }
 
 function onGridCellMouseEnter(event) {
-    event.target.style.backgroundColor = "black";
 }
 
-generateBoard(16, 16);
+generateButton.addEventListener("click", () => {
+    let size = prompt("Enter size: ", 16);
+
+    if (isNaN(size) || size <= 0) return;
+
+    if (size > 100) {
+        console.error("Size mustn't exceed 100.");
+        return;
+    }
+
+    generateBoard(size);
+});
+
+generateBoard(16);
